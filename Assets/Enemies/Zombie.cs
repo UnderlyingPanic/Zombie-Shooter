8055f9b8d547799d5547fa7109c107b10216208c
NATASHA\Stefan <stefanwicks@hotmail.co.uk>
2017-06-09 22:13:58 +0100

    Partially implemented shooting

---
+++ Assets/Enemies/Zombie.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UnityStandardAssets.Characters.ThirdPerson {
+
+    [RequireComponent(typeof(ThirdPersonCharacter))]
+    public class Zombie : MonoBehaviour
+    {
+
+        private Animator animator;
+        private Player player;
+        private float distanceToPlayer;
+        private ThirdPersonCharacter character;
+        private AICharacterControl AIControl;
+        private Scorekeeper scorekeeper;
+
+        public float damage;
+        public float bigDamageMultiplier;
+        public float attackRange = 0f;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+            character = GetComponent<ThirdPersonCharacter>();
+            AIControl = GetComponent<AICharacterControl>();
+            player = GameObject.FindObjectOfType<Player>();
+            scorekeeper = FindObjectOfType<Scorekeeper>();
+            print(player);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            distanceToPlayer = Vector3.Distance(this.transform.position, player.transform.position);
+
+            if (distanceToPlayer <= attackRange)
+            {
+                if (AIControl.attacking == false)
+                {
+                    animator.SetTrigger("Trigger Attack");
+                }
+            }
+
+        }
+        public void DealDamage()
+        {
+            if (distanceToPlayer <= attackRange)
+            {
+                player.TakeDamage(damage);
+            }
+            else
+            {
+                scorekeeper.timesDodged++;
+            }
+
+        }
+        public void DealBigDamage()
+        {
+            if (distanceToPlayer <= attackRange)
+            {
+                player.TakeDamage(damage * bigDamageMultiplier);
+            }
+            else
+            {
+                scorekeeper.timesDodged++;
+            }
+
+        }
+    }
+}
---
+++ Assets/Player/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private float maxHealthPoints =100f;
+    private float currentHealthPoints= 100f;
+    public float healthAsPercentage
+    {
+        get { return currentHealthPoints / (float)maxHealthPoints; }
+    }
+    private Bloodsplat bloodSplat;
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletSpeed = 6f;
+
+    // Use this for initialization
+    void Start () {
+        bloodSplat = GetComponentInChildren<Bloodsplat>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+            
+        
+	}
+
+    public void TakeDamage(float damage)
+    {
+        currentHealthPoints -= damage;
+        bloodSplat.SpawnBloodsplat();
+    }
+
+    public void Fire()
+    {
+        
+       var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation) as GameObject;
+       bullet.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * bulletSpeed;
+
+      Destroy(bullet, 2.0f);
+    }
+}
