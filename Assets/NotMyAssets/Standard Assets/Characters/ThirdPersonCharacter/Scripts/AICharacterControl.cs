e5ac9e7cbb723d0d8001a8710c0d0ca981380635
NATASHA\Stefan <stefanwicks@hotmail.co.uk>
2017-06-09 20:47:30 +0100

    Initial Commit - AI, Bloodsplat, Player Damage implemented.

---
+++ Assets/NotMyAssets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -0,0 +1,66 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+    [RequireComponent(typeof(ThirdPersonCharacter))]
+    public class AICharacterControl : MonoBehaviour
+    {
+        public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+        public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+        public Transform originalTarget;
+        private Transform m_target;// target to aim for
+        public bool attacking;
+
+        private void Start()
+        {
+            // get the components on the object we need ( should not be null due to require component so no need to check )
+            agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+            agent.updateRotation = false;
+            agent.updatePosition = true;
+        }
+
+
+
+        private void Update()
+        {
+            if (m_target != null)
+                agent.SetDestination(m_target.position);
+
+            if (attacking)
+            {
+                this.m_target = this.transform;
+            } else
+            {
+                this.m_target = originalTarget;
+            }
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                character.Move(agent.desiredVelocity, false, false);               
+            }
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+        }
+
+
+        public void SetTarget(Transform target)
+        {
+            this.m_target = target;
+        }
+
+        public void SetAttackingTrue()
+        {
+            attacking = true;
+        }
+        public void SetAttackingFalse()
+        {
+            attacking = false;
+        }
+    }
+}
