c74984aa7712cef276c9cb7fc77e071663a4a4b0
NATASHA\Stefan <stefanwicks@hotmail.co.uk>
2017-06-10 09:57:38 +0100

    Balance Tweaks

--- Assets/Z_Standard Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ Assets/Z_Standard Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -18,6 +18,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             // get the components on the object we need ( should not be null due to require component so no need to check )
             agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
             character = GetComponent<ThirdPersonCharacter>();
+            originalTarget = FindObjectOfType<Player>().transform;
 
             agent.updateRotation = false;
             agent.updatePosition = true;
